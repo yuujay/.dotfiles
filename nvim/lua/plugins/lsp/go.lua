49cc4f17edf8649ecd6f3819d0cedbb29557a71d
gumamahe <gumamahe@mathworks.com>
2022-01-22 01:49:48 -0500

    removing language specific lsp files

--- nvim/lua/plugins/lsp/go.lua
+++
@@ -1,2 +0,0 @@
--- go install golang.org/x/tools/gopls@latest
-require('lspconfig').gopls.setup{}
--- nvim/lua/plugins/lsp/html.lua
+++
@@ -1,5 +0,0 @@
--- npm i -g vscode-html-languageserver-bin
--- npm i -g emmet-ls
-
-require('lspconfig').html.setup({
-})
