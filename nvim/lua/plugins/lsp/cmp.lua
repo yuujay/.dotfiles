94d23de2d681dae0d0019a775a25621c60090961
gumamahe <gumamahe@mathworks.com>
2022-01-23 12:03:21 -0500

    linter fixes

--- nvim/lua/plugins/lsp/cmp.lua
+++ nvim/lua/plugins/lsp/cmp.lua
@@ -3,53 +3,55 @@ local lspkind = require('lspkind')
 local luasnip = require('luasnip')
 
 lspkind.init()
-require("luasnip/loaders/from_vscode").lazy_load()
+require('luasnip/loaders/from_vscode').lazy_load()
 
 -- Most configs are from `nvim-cmp` github page and
 -- TJ's [youtube video](https://www.youtube.com/watch?v=_DnmphIwnjo&ab_channel=TJDeVries)
 -- https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/after/plugin/completion.lua
 
-vim.o.completeopt = "menu,menuone,noselect,noinsert"
+vim.o.completeopt = 'menu,menuone,noselect,noinsert'
 
 cmp.setup ({
     documentation = true,
     snippet = {
       expand = function(args)
-        require("luasnip").lsp_expand(args.body)
+        require('luasnip').lsp_expand(args.body)
       end,
     },
     mapping = {
-        ["<C-d>"] = cmp.mapping.scroll_docs(-2),
-        ["<C-f>"] = cmp.mapping.scroll_docs(2),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-y>"] = cmp.mapping(
+        ['<C-d>'] = cmp.mapping.scroll_docs(-2),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-f>'] = cmp.mapping.scroll_docs(2),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-y>'] = cmp.mapping(
           cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
           },
-          { "i", "c" }
+          { 'i', 'c' }
         ),
     },
     sources = {
-        { name = "luasnip" },
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "buffer", keyword_length = 4 },
-        { name = "path" },
-        -- { name = "gh_issues" },
+        { name = 'luasnip' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'buffer', keyword_length = 4 },
+        { name = 'path' },
+        -- { name = 'gh_issues' },
     },
     formatting = {
         fields = {'kind', 'abbr', 'menu'},
         with_text = true,
         format = lspkind.cmp_format {
             menu = {
-                buffer = "[buf]",
-                nvim_lsp = "[LSP]",
-                nvim_lua = "[api]",
-                path = "[path]",
-                luasnip = "[snip]",
-                -- gh_issues = "[issues]",
+                buffer = '[buf]',
+                nvim_lsp = '[LSP]',
+                nvim_lua = '[api]',
+                path = '[path]',
+                luasnip = '[snip]',
+                -- gh_issues = '[issues]',
             },
         },
     },
