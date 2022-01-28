4ad803bf5b900ae2eb61978e7a287587e21c3a4a
gumamahe <gumamahe@mathworks.com>
2022-01-28 18:32:20 -0500

    Set some fugitive augroup

--- nvim/lua/general/augroup.lua
+++ nvim/lua/general/augroup.lua
@@ -14,6 +14,13 @@
         autocmd FileType gitcommit setlocal spellsuggest+=10
     augroup end
 
+    augroup _vim_fugitive
+        autocmd!
+        autocmd FileType fugitive nnoremap <buffer> <silent> q :<C-U>call fugitive#DiffClose()<CR>
+        autocmd FileType fugitive nnoremap <buffer> <silent> d :<C-U>execute <SNR>131_StageInline('toggle',line('.'),v:count)<CR>
+        autocmd FileType fugitive nnoremap <buffer> <silent> <ESC> :q<CR>
+    augroup end
+
     augroup _markdown
         autocmd!
         autocmd FileType markdown setlocal wrap
