07365b5fdf8092d55346bcd6a1ccc31a235bf556
gumamahe <gumamahe@mathworks.com>
2022-02-06 16:12:32 -0500

    More info on nvim README

--- nvim/lua/general/commands.lua
+++ nvim/lua/general/commands.lua
@@ -5,7 +5,7 @@ Command.cmd({
     'highlight NvimTreeFolderIcon guibg=blue',
     "match RedundantSpaces /\\s\\+$\\| \\+\\ze\\t/",
     'highlight ColorColumn ctermbg=blue guibg=blue',
-    -- 'highlight CursorLine guibg=#303000 ctermbg=236 cterm=none term=none',
+    'highlight CursorLine guibg=#303000 ctermbg=236 cterm=none term=none',
     -- The settings below are not required since themes and colorschemes are now in use
     -- 'highlight SignColumn ctermbg=black guibg=black',
 })
