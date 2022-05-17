dec6074b6ec9aba61d7ccbfa829cd6d52abb8903
gumamahe <gumamahe@mathworks.com>
2022-05-17 12:02:53 -0400

    add `fkl` and fix `fdl`

--- functions/fdl.fish
+++ functions/fdl.fish
@@ -16,3 +16,5 @@ function fdl
              --bind "ctrl-i:+abort" \
              --bind "ctrl-b:execute:$_enter_container"
 end
+
+
---
+++ functions/fkl.fish
@@ -0,0 +1,17 @@
+function fkl
+    set _pod_name "echo {+2} | tr -d \"\n\""
+
+    set _pod_logs "echo {} | xargs -oI{} bash -c \"kubectl logs {+2} -n  {+1}\""
+    set _copy_pod_name "echo {+2} | tr -d \"\n\" | pbcopy"
+    set _log_in_less "$_pod_logs | less -R"
+    set _enter_pod "echo {} | xargs -oI{} bash -c \"kubectl exec -it {+2} sh -n  {+1}\""
+
+    kubectl get pods --all-namespaces | \
+        awk '( NR>1 )' | \
+        fzf  --preview=$_pod_logs \
+             --header "<space>: toggle preview | <enter>: view logs | <C-b>: Bash into pod | <C-i>: Copy pod name " \
+             --bind "enter:execute:$_log_in_less" \
+             --bind "ctrl-i:execute:$_copy_pod_name" \
+             --bind "ctrl-i:+abort" \
+             --bind "ctrl-b:execute:$_enter_pod"
+end
