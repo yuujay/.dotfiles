2d7bb99506b99dee545db223356bf0eaee256bab
gumamahe <gumamahe@mathworks.com>
2022-09-30 13:16:30 -0400

    Add fglb to fuzzy diff between specific branches

---
+++ functions/fglb.fish
@@ -0,0 +1,25 @@
+function fglb
+    set _viewGitLogLine "echo {} | \
+    grep -o '[a-f0-9]\{7\}' | \
+    head -1 | \
+    xargs -I % sh -c 'git show --color=always %'"
+
+    set _gitLogLineToHash "echo -n {} | grep -o '[a-f0-9]\{7\}' | head -1 | tr -d \"\n\""
+
+    git log \
+        --color=always \
+        --graph \
+        --abbrev-commit \
+        --first-parent $argv \
+        --pretty=format:"%C(dim)%C(yellow)%h%C(reset) %C(auto)%d %C(brightgreen)%s%  %C(auto)%cN %ar"|
+        fzf -i -e +s \
+            --header "<space>: toggle preview | <enter>: view commit diff | <C-i>: copy commit hash" \
+            --reverse \
+            --tiebreak=index \
+            --no-multi \
+            --border \
+            --preview=$_viewGitLogLine \
+            --bind "enter:execute:$_viewGitLogLine | less -R" \
+            --bind "ctrl-i:execute-silent:$_gitLogLineToHash | pbcopy" \
+            --bind "ctrl-i:+abort"
+end
