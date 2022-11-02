883663470149158ce43e461b44a0fb19bdd9a41a
gumamahe <gumamahe@mathworks.com>
2022-11-02 15:59:24 -0400

    add gpg config to git

--- nvim/lua/install-plugins.lua
+++ nvim/lua/install-plugins.lua
@@ -70,6 +70,8 @@ require "paq" {
     -- Nvim clients outside of terminal
     {'glacambre/firenvim', run='firenvim#install(1)'},
 
+    'epwalsh/obsidian.nvim',  -- For obsidian
+
     -- "alvan/vim-closetag",
     -- "b3nj5m1n/kommentary",
     -- "rmagatti/auto-session",
@@ -102,3 +104,24 @@ require('github-theme').setup({
 
 require('hop').setup()
 
+-- Most of the config comes from https://github.com/epwalsh/obsidian.nvim
+require("obsidian").setup({
+  dir = "~/my-vault",
+  completion = {
+    nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
+  }
+    -- note_id_func = function(title)
+    -- -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- local suffix = ""
+    -- if title ~= nil then
+    --   -- If title is given, transform it into valid file name.
+    --   suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    -- else
+    --   -- If title is nil, just add 4 random uppercase letters to the suffix.
+    --   for _ in 1, 4 do
+    --     suffix = suffix .. string.char(math.random(65, 90))
+    --   end
+    -- end
+    -- return tostring(os.time()) .. "-" .. suffix
+  -- end
+}) 
