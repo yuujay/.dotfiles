3974ffff13f328f1d0174edb2182a794619428ae
yuujay <gowtham.uj@gmail.com>
2022-12-24 16:14:55 -0500

    LSP keybindings

--- nvim/lua/plugins/lsp/lsp-zero.lua
+++ nvim/lua/plugins/lsp/lsp-zero.lua
@@ -1,8 +1,18 @@
+-- This file contains configs for LSP which includes config of multiple plugin
+-- 1. lsp-zero
+-- 2. nvim-cmp
+-- 3. lspkind
+-- and some common vim diagnostics (Check if this can be moved to a different file)
+-- Insipired from 
+--  1. https://github.com/ThePrimeagen/init.lua/blob/master/after/plugin/lsp.lua
+--  2. https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+
 -- lsp-zero config
 local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.set_preferences({
+    suggest_lsp_servers = true,
     sign_icons = {
         error = " ",
         warn = " ",
@@ -23,8 +33,6 @@ lsp.ensure_installed({
     'tsserver',
     'yamlls',
 })
-
-lsp.setup()
 
 -- Small VSCode like pictograms for nvim LSP
 local lspkind = require('lspkind')
@@ -76,6 +84,51 @@ lsp.setup_nvim_cmp({
         },
     },
 })
+
+-- LSP settings.
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+  -- We create a function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- Lesser used LSP functionality
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+  -- NOTE: Uncomment this if and when a use case arise.
+  -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  -- nmap('<leader>wl', function()
+  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end, '[W]orkspace [L]ist Folders')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
+end
+
+lsp.setup()
 
 -- Diagnostics 
 vim.diagnostic.config({
