0c80268f7a043e3c915b9d626fcbbf4cf5554945
yuujay <gowtham.uj@gmail.com>
2022-12-29 21:36:57 -0500

    add borders to floating windows

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,5 +1,29 @@
+-- set border for diagnostics window
+vim.cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+local border = {
+    {"╭", "FloatBorder"},
+    {"─", "FloatBorder"},
+    {"╮", "FloatBorder"},
+    {"│", "FloatBorder"},
+    {"╯", "FloatBorder"},
+    {"─", "FloatBorder"},
+    {"╰", "FloatBorder"},
+    {"│", "FloatBorder"},
+}
+
+local open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or border
+  return open_floating_preview(contents, syntax, opts, ...)
+end
+
+-- Set border for mason float window
 require("mason").setup({
     ui = {
+        border = 'rounded',
         icons = {
             package_installed = "✓",
             package_pending = "➜",
