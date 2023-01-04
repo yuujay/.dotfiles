d92f0a4449413d027aec242dd416b94522120e36
Gowtham <gumamahe@mathworks.com>
2023-01-04 17:29:43 -0500

    Move window management to `yabai` and `skhd` from hammerspoon (#43)
    * clean up some more hammerspoon code
    
    * some skhd hotkeys

--- hammerspoon/.hammerspoon/apps.lua
+++ hammerspoon/.hammerspoon/apps.lua
@@ -1,17 +1,21 @@
--- App Sizing
-hs.hotkey.bind(HYPER, 'm', toggleMaximizedWindow)
-hs.hotkey.bind(HYPER, 'c', centerScreen)
-hs.hotkey.bind(HYPER, 'f', fullScreen)
-hs.hotkey.bind(HYPER, 'r', sizeRestore)
+local function openAppBinding(name)
+    return function()
+        if not hs.application.launchOrFocus(name) then
+          hs.alert.show(name..' - Not found')
+        end
+        if name == 'Finder' then
+            hs.appfinder.appFromName(name):activate()
+        end
+    end
+end
 
--- move apps between monitors
-hs.hotkey.bind(CMD_CTRL, 'h', moveAppLeft)
-hs.hotkey.bind(CMD_CTRL, 'l', moveAppRight)
+local function helpMenu()
+  hs.alert.show(' Alt-C = Chrome \n Alt-D = DBeaver \n Alt-F = Finder \n Alt-I = IntelliJ \n Alt-M = Teams \n Alt-S = Screen Sharing \n Alt-T = iTerm \n Alt-V VSCode')
+end
 
 -- Shortcuts to open the app
 -- NOTE: Update `helpMenu` in `shortcuts` when anything in this block changes
 hs.hotkey.bind({'alt'}, '0', helpMenu)
--- hs.hotkey.bind({'alt'}, 'B', openAppBinding('Safari'))
 hs.hotkey.bind({'alt'}, 'C', openAppBinding('Google Chrome'))
 hs.hotkey.bind({'alt'}, 'D', openAppBinding('DBeaver'))
 hs.hotkey.bind({'alt'}, 'F', openAppBinding('Finder'))
@@ -19,9 +23,8 @@ hs.hotkey.bind({'alt'}, 'I', openAppBinding('IntelliJ IDEA'))
 hs.hotkey.bind({'alt'}, 'M', openAppBinding('Microsoft Teams'))
 hs.hotkey.bind({'alt'}, 'N', openAppBinding('Obsidian'))
 hs.hotkey.bind({'alt'}, 'O', openAppBinding('Microsoft Outlook'))
--- hs.hotkey.bind({'alt'}, 'R', openAppBinding('Reminders'))
 hs.hotkey.bind({'alt'}, 'S', openAppBinding('Screen Sharing'))
-hs.hotkey.bind({'alt'}, 'T', openAppBinding('iTerm'), fullScreen)
+hs.hotkey.bind({'alt'}, 'T', openAppBinding('iTerm'))
 hs.hotkey.bind({'alt'}, 'V', openAppBinding('Visual Studio Code'))
 hs.hotkey.bind({'alt'}, 'X', openAppBinding('Firefox'))
 hs.hotkey.bind({'alt'}, 'Z', openAppBinding('KeyCastr'))
