81d98870ac46457a0e7518b294ef292b3444eb42
Gowtham Jaganathan <gumamahe@mathworks.com>
2023-02-08 16:05:56 -0500

    fish updates

--- fish/.config/fish/mw/env.fish
+++ fish/.config/fish/mw/env.fish
@@ -1,13 +1,14 @@
-set -x GOFLAGS -buildvcs=false
 set -x GO111MODULE "on"
 set -x GOARCH "amd64"
 set -x GOBIN ""
+set -x GOFLAGS -buildvcs=false
+set -x GOMODCACHE "/home/gumamahe/go/pkg/mod"
 set -x GONOPROXY "github.mathworks.com"
 set -x GOOS ""
 set -x GOPATH "/home/gumamahe/go"
-set -x GOROOT ""
 set -x GOPRIVATE "golang.dhcp,github.mathworks.com"
 set -x GOPROXY "http://iat-go-proxy-prod-01:7000/go-proxy"
+set -x GOROOT "/mathworks/hub/3rdparty/internal/9285011/maci64/golang"
 set -x GOTOOLDIR "/home/gumamahe/go/pkg/tool/darwin_amd64/"
 set -x MW_ARCH ""
 set GEM_HOME "$HOME/.gem/ruby/2.6.0"
