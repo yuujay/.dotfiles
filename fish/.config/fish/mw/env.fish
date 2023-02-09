66f3a6c1c63c2248eae3f0c996a5cc3f6d0c2c1a
Gowtham Jaganathan <gumamahe@mathworks.com>
2023-02-09 16:42:56 -0500

    add vdi to gitconfig and set gpg prog for git

--- fish/.config/fish/mw/env.fish
+++ fish/.config/fish/mw/env.fish
@@ -1,18 +1,18 @@
-set -x GO111MODULE "on"
-set -x GOARCH "amd64"
-set -x GOBIN ""
-set -x GOFLAGS -buildvcs=false
-set -x GOMODCACHE "/home/gumamahe/go/pkg/mod"
-set -x GONOPROXY "github.mathworks.com"
-set -x GOOS ""
-set -x GOPATH "/home/gumamahe/go"
-set -x GOPRIVATE "golang.dhcp,github.mathworks.com"
-set -x GOPROXY "http://iat-go-proxy-prod-01:7000/go-proxy"
-set -x GOROOT "/mathworks/hub/3rdparty/internal/9285011/maci64/golang"
-set -x GOTOOLDIR "/home/gumamahe/go/pkg/tool/darwin_amd64/"
-set -x MW_ARCH ""
-set GEM_HOME "$HOME/.gem/ruby/2.6.0"
+set -gx GO111MODULE "on"
+set -gx GOARCH "amd64"
+set -gx GOBIN ""
+set -gx GOFLAGS -buildvcs=false
+set -gx GOMODCACHE "/home/gumamahe/go/pkg/mod"
+set -gx GONOPROXY "github.mathworks.com"
+set -gx GOOS ""
+set -gx GOPATH "/home/gumamahe/go"
+set -gx GOPRIVATE "golang.dhcp,github.mathworks.com"
+set -gx GOPROXY "http://iat-go-proxy-prod-01:7000/go-proxy"
+set -gx GOROOT "/mathworks/hub/3rdparty/internal/9285011/maci64/golang"
+set -gx GOTOOLDIR "/home/gumamahe/go/pkg/tool/darwin_amd64/"
+set -gx MW_ARCH ""
+set -gx GEM_HOME "$HOME/.gem/ruby/2.6.0"
 
-set -x UID (id -u)
-set -x GID (id -g)
+set -gx UID (id -u)
+set -gx GID (id -g)
 
