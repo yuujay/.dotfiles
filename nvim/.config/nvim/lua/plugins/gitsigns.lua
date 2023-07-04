f4a55cf497e99db85c33fef3cbadf0828f066506
Gowtham Jaganathan <gumamahe@mathworks.com>
2023-07-04 11:41:53 -0400

    add blame options

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -5,6 +5,7 @@ return {
     keys = {
         {"<leader>gj", "<cmd>Gitsigns next_hunk<CR>", desc="Git Next Hunk"},
         {"<leader>gk", "<cmd>Gitsigns prev_hunk<CR>", desc="Git Previous Hunk"},
+        {"<leader>gb", "<cmd>Gitsigns blame_line<CR>", desc="Git blame line"},
         {"<leader>gu", "<cmd>Gitsigns reset_hunk<CR>", desc="Git Undo Hunk"},
         {"<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", desc="Git Stage Hunk"},
         {"<leader>gh", "<cmd>Gitsigns preview_hunk<CR>", desc="Git Preview Hunk"},
