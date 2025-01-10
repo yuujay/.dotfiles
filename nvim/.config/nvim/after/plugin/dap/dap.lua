384daa9c081c5bb0dc68b0b93d3afbaea24b50ca
Gowtham Jaganathan <gumamahe@mathworks.com>
2025-01-10 15:20:32 -0500

    Working dap

--- nvim/.config/nvim/after/plugin/dap/dap.lua
+++ nvim/.config/nvim/after/plugin/dap/dap.lua
@@ -4,10 +4,10 @@ if not ok then return end
 local dap = require('dap')
 local ui = require('dapui')
 
-require("dapui").setup()
-require("dap-go").setup()
+require('dapui').setup()
+require('dap-go').setup()
 
-require("nvim-dap-virtual-text").setup()
+require('nvim-dap-virtual-text').setup()
 
 -- Diagnostics setting
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "goto previous diagnostics" })
@@ -25,15 +25,7 @@ vim.diagnostic.config({
 })
 
 -- Debugging
-vim.keymap.set("n", "<leader>5", dap.continue)
-vim.keymap.set("n", "<leader>3", dap.step_over)
-vim.keymap.set("n", "<leader>2", dap.step_into)
-vim.keymap.set("n", "<leader>9", dap.step_out)
-vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
-vim.keymap.set("n", "<leader>B", dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')))
-vim.keymap.set("n", "<leader>lp", dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')))
-vim.keymap.set("n", "<leader>dr", dap.repl.open)
-vim.fn.sign_define("DapBreakpoint", { text = "⬢", texthl = "Yellow", linehl = "", numhl = "Yellow" })
+vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "", numhl = "Yellow" })
 vim.fn.sign_define("DapStopped", { text = "▶", texthl = "Green", linehl = "ColorColumn", numhl = "Green" })
 
 dap.listeners.before.attach.dapui_config = function()
@@ -48,3 +40,20 @@ end
 dap.listeners.before.event_exited.dapui_config = function()
   ui.close()
 end
+
+
+-- Eval var under cursor. See if this is need when using ghost/virtual text
+-- vim.keymap.set("n", "<space>?", function()
+--   require("dapui").eval(nil, { enter = true })
+-- end)
+
+vim.keymap.set("n", "<leader>5", dap.continue)
+vim.keymap.set("n", "<leader>6", dap.clear_breakpoints)
+vim.keymap.set("n", "<leader>7", dap.disconnect)
+vim.keymap.set("n", "<leader>3", dap.step_over)
+vim.keymap.set("n", "<leader>2", dap.step_into)
+vim.keymap.set("n", "<leader>9", dap.step_out)
+vim.keymap.set("n", "<leader>0", dap.restart)
+vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+vim.keymap.set("n", "<leader>dr", dap.repl.open)
+vim.keymap.set("n", "<leader>B", function() dap.set_breakpoint(nil, nil, vim.fn.input('Breakpoint condition: ')) end )
