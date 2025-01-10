26ab3029c4625dc971940cb8e5664d62d4b30d8a
Gowtham Jaganathan <gumamahe@mathworks.com>
2025-01-09 20:26:28 -0500

    gopls

--- nvim/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,9 +1,9 @@
-print('inside plugin.lsp.lspconfig')
 return {
     {
         'neovim/nvim-lspconfig',
         config = function()
             require('lspconfig').lua_ls.setup {}
+            require('lspconfig').gopls.setup {}
         end,
     }
 }
